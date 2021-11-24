b7b849e1e4058e850a4db7cb3e85c5668bb524e7
Fabiane Albuquerque <fabiane.albuquerque@rebase.com.br>
2021-11-24 10:55:25 -0300

    Refactored spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,18 +2,16 @@ require 'simplecov'
 
 SimpleCov.start
 
-require './controllers/users_controller'
-require './controllers/home_controller'
-require './controllers/sessions_controller'
-require 'rspec'
-require 'rack/test'
 require 'pg'
-require_relative '../config/data_base'
-require_relative './support/user_helper'
-require_relative './support/json_helper'
+require 'rack/test'
+require 'rspec'
+
+require './dependencies/rspec'
 
 ENV['DATABASE'] = 'test'
-BCrypt::Engine.cost = 6
+
+BCRYPT_COST = 6.freeze
+BCrypt::Engine.cost = BCRYPT_COST
 
 def app
   Sinatra::Application
